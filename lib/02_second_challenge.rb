0999baa7eec8c95232b603adffb5814c84ebb1f7
Riley Iverson <rmiverson99@gmail.com>
2020-10-22 02:10:39 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,12 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-        :patriarch => {
-
-        },
+        :patriarch => { },
         :matriarch => {
-          :name => "Lady Montague",
-          :age => "54"
+
         },
         :hero => {
           :name => "Romeo",
