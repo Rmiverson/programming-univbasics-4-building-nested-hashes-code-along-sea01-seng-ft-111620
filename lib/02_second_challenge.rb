49a3568d28ced6288038df0bdb86c0c0ba8cb1e5
Riley Iverson <rmiverson99@gmail.com>
2020-10-22 02:11:58 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -13,9 +13,7 @@ def second_challenge
 
         },
         :heroine => {
-          :name => "Juliet",
-          :age => "15",
-          :status => "alive"
+
         },
         :heroine_friends => []
     }
